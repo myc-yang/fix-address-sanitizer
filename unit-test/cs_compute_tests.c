c8519e7d28a20c600505e4fb82fc9177fc5c5c95
Michael Yang <michael.y.yang@nasa.gov>
2025-05-07 11:39:45 -0400

    Fixed unit-test/cs_compute_tests.c for the address-sanitizer

--- unit-test/cs_compute_tests.c
+++ unit-test/cs_compute_tests.c
@@ -1455,11 +1455,16 @@ void CS_RecomputeAppChildTask_Test_Nominal(void)
 
 void CS_RecomputeAppChildTask_Test_CouldNotGetAddress(void)
 {
+    int i;
     CS_Res_App_Table_Entry_t RecomputeAppEntry;
-    CS_Def_App_Table_Entry_t DefAppTbl[10];
+    CS_Def_App_Table_Entry_t DefAppTbl[CS_MAX_NUM_APP_TABLE_ENTRIES];
 
     memset(&RecomputeAppEntry, 0, sizeof(RecomputeAppEntry));
-    memset(&DefAppTbl, 0, sizeof(DefAppTbl));
+    memset(DefAppTbl, 0, sizeof(DefAppTbl));
+
+    for (i = 0; i < CS_MAX_NUM_APP_TABLE_ENTRIES; i++){
+        strncpy(DefAppTbl[i].Name, "blank", 10);
+    }
 
     CS_AppData.RecomputeAppEntryPtr = &RecomputeAppEntry;
     CS_AppData.DefAppTblPtr         = DefAppTbl;
@@ -1637,7 +1642,7 @@ void CS_RecomputeTablesChildTask_Test_Nominal(void)
 void CS_RecomputeTablesChildTask_Test_CouldNotGetAddress(void)
 {
     CS_Res_Tables_Table_Entry_t RecomputeTablesEntry;
-    CS_Def_Tables_Table_Entry_t DefTablesTbl[10];
+    CS_Def_Tables_Table_Entry_t DefTablesTbl[CS_MAX_NUM_TABLES_TABLE_ENTRIES];
 
     memset(&RecomputeTablesEntry, 0, sizeof(RecomputeTablesEntry));
     memset(&DefTablesTbl, 0, sizeof(DefTablesTbl));
